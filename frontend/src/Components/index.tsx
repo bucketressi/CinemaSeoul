f455931d53d1013e626b703abba31a45a2fc8764
heeeun <gmldms784@naver.com>
2021-05-11 01:54:19 +0900

    make moviecard

--- frontend/src/Components/index.tsx
+++ frontend/src/Components/index.tsx
@@ -3,3 +3,4 @@ export { default as Header } from './Header';
 export { default as AdminHeader } from './AdminHeader';
 export { default as Layout } from './Layout';
 export { default as AdminLayout } from './AdminLayout';
+export { default as MovieCard } from './MovieCard';
--- frontend/src/Pages/User/Main.tsx
+++ frontend/src/Pages/User/Main.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
 
-import { Grid } from '@material-ui/core';
-
-import { Layout } from '../../Components';
+import { Layout, MovieCard } from '../../Components';
+import "../../scss/pages/main.scss";
 
 const Main = () => {
 	// 메인 페이지
 	const a = 1;
+	const movi_name = "분노의 질주, 더 얼티메이트";
+	const bookrecord = 49.4;
+	const open_date = new Date("2021-05-23");
+	const avi_age = 19;
+
 	return (
 		<Layout>
-			<div>컨텐츠</div>
+			<div className="movie-list">
+				{
+					[1,2,3].map((i) => 
+						<MovieCard
+							key = {i}
+							movi_name = {movi_name}
+							bookrecord = {bookrecord}
+							open_date = {open_date}
+							avi_age = {avi_age}
+						/>)
+				}
+			</div>
 		</Layout>
 	);
 }
