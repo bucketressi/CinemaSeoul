6a9595e198caf30bee2bc6c6236e019ac7b52169
howtolivelikehuman <yoich_@naver.com>
2021-05-23 21:11:38 +0900

    Edit Info

--- backend/cinemaseoul/src/main/java/com/uos/cinemaseoul/dto/movie/MovieListInfoDto.java
+++ backend/cinemaseoul/src/main/java/com/uos/cinemaseoul/dto/movie/MovieListInfoDto.java
@@ -12,6 +12,8 @@ public class MovieListInfoDto {
 
     private int movi_id;
     private String movi_name;
+    private String avai_age;
+    private String open_date;
     private float rating;
     private int accu_audi;
     private byte[] image;
