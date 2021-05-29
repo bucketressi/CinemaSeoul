2928e462cf8fc45ebff0d24c6f780d90e717792d
gmldms784 <gmldms784@naver.com>
2021-05-29 21:07:58 +0900

    상영일정 추가 모달 생성 - 상영관 먼저해야함

--- frontend/src/Pages/Admin/AdminShowSchedule.tsx
+++ frontend/src/Pages/Admin/AdminShowSchedule.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { PageTitle } from '../../Components';
 import { useHistory } from 'react-router-dom';
-import { ListItem, ListItemAvatar, Avatar, ListItemSecondaryAction, ListItemText, Checkbox, Switch } from '@material-ui/core';
+import { ListItem, ListItemText, Switch, Button, TextField, Radio, RadioGroup, FormControl, FormLabel, FormControlLabel } from '@material-ui/core';
 import { ShowScheduleType } from '../../Main/Type';
 import { useShowScheduleListState } from '../../Main/ShowScheduleModel';
-import { Schedule } from '@material-ui/icons';
+import { ModalComponent } from '../../Components';
+import "../../scss/pages/adminshowschedule.scss";
 
 const AdminShowSchedule = () => {
 	const history = useHistory();
@@ -27,18 +28,31 @@ const AdminShowSchedule = () => {
 		setMode(event.target.checked);
 	}
 
+	/* 상영일정 추가 */
+	const [openModal, setOpenModal] = useState<boolean>(false);
+
+	const addShowSchedule = () => {
+		console.log("상영일정 추가");
+	}
+
+
 	return (
 		<div>
 			<PageTitle title="상영일정 페이지" isButtonVisible={true} />
 			<div className="schedule-con">
-				<div>
-					<div>{mode?"표" : "리스트"}</div>
-					<Switch
-						checked={mode}
-						onChange={handleModeChange}
-						color="primary"
-						name={mode?"표" : "리스트"}
-					/>
+				<div className="schedule-header">
+					<div className="switch-con">
+						<div>{mode?"표" : "리스트"}</div>
+						<Switch
+							checked={mode}
+							onChange={handleModeChange}
+							color="primary"
+							name={mode?"표" : "리스트"}
+						/>
+					</div>
+					<div className="save-btn">
+						<Button variant="contained" color="primary" onClick={() => setOpenModal(true)}>추가</Button>
+					</div>
 				</div>
 				{
 					mode?
@@ -58,6 +72,17 @@ const AdminShowSchedule = () => {
 						})
 				}
 			</div>
+			<ModalComponent
+				open={openModal}
+				setOpen={setOpenModal}
+				title="상영일정 추가"
+				button="추가"
+				buttonOnClick={addShowSchedule}
+			>
+				<div className="add-container">
+					ㅗㅑ
+				</div>
+			</ModalComponent>
 		</div>
 	);
 }
